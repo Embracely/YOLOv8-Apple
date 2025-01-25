184402f26031e148c7b3670b6b8913764231c154
Yichen Lian <110317279+Embracely@users.noreply.github.com>
2025-01-25 06:39:17 +0000

    Update train.py

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
  
 if __name__ == '__main__':
-    model = YOLO('yolo-apple.pt')  # 从YAML建立一个新模型，可在同一路径下更换带训练模型的yaml文件
+    model = YOLO('yolov8-apple.pt')  # 从YAML建立一个新模型，可在同一路径下更换带训练模型的yaml文件
     model.train(data=r'E:\UOB\MV\project\ultralytics-main\wheat.yaml',
                 cache=False,
                 imgsz=640,
